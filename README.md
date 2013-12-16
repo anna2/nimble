a59b24b4dbc1e32fd64f2baefe7e51aff8922cdb
Anna Sheaffer <annasheaffer@gmail.com>
2013-12-16 14:45:53 -0500

    Initial commit; working GET and POST routing

---
+++ README.md
@@ -0,0 +1,3 @@
+WIP
+
+A Ruby web framework. Uses Rack to handle HTTP.
---
+++ rack_request.rb
@@ -0,0 +1,15 @@
+class RackRequest
+  def call(env)
+    request = Rack::Request.new(env)
+    block = App.routes[request.request_method][request.path_info]
+    if block
+      puts "Block: #{block}"
+      puts "PARAMS: #{request.params}"
+      params = request.params
+      response = block.call(params)
+      [200, {}, [response]]
+    else
+      [404, {}, ["Page not found"]]
+    end
+  end
+end
